f66b632988879170355a1a3f6019749f36cca411
zurabkharaishvili9-svg <zurabkharaishvili9@gmail.com>
2026-03-07 19:53:24 +0400

    all

---
+++ day 8/class/class_work.py
@@ -0,0 +1,7 @@
+age = int( input("enter your age:"))
+#integera
+is_he_18_or_more= 18<=age
+print(is_he_18_or_more)
+#bool
+
+print( True or False)
---
+++ day 9/home work/work.py
@@ -0,0 +1,64 @@
+a1=5
+a2=2
+a3=6
+a4=3
+a6= 1
+
+b1=2.3
+b2=4.3
+b3=4.2
+b4=12.3
+b5=32.23
+print(type(32.3))
+c1="ga" 
+c2='42' 
+c3= "fs" 
+c4=  "sd"
+c5="gs"
+
+#გვაქვს ნასწავლი  შვიდი ოპერატორ 
+
+print(b1+b2)
+print(b2-b1)
+print(b2*b1)
+print(b2/b1)
+print(b2**b1)
+print(b1//b2)
+print(b1%b2)
+
+print(5>3)
+print(10>3)
+print(3>5)
+print(2>9)
+print(20.32>20.31)
+
+print(5<2)
+print(4<8)
+print(10<21)
+print(11<12)
+print(25<4)
+
+print(2<=2)  
+print(10<=15)
+print(13<=23)
+print(15<=11)
+print(14<=12)
+
+print(21>=23)
+print(11>=5)
+print(53>=23)
+print(15>=15)
+print(3>=2)
+      
+#bool არის ლოგიკური ოპერატორი 
+
+y=int(input("enter any number: "))
+x= int(input("enter any number: "))
+number= y>x
+print(number)
+number1=y>10
+number3=x>20
+print(number1)
+print(number3)
+number2=100>x<999
+number4=100>y<999
